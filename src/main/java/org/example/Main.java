52d6c6875bc2eece1aa995e0c325ea295ecaa685
Donna Lemon <donnalemon1@gmail.com>
2022-04-22 13:54:38 -0400

    Use an ArrayList to store the letters....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.print("Enter a sentence:");
-        int numWords = 4;
-        var scanner = new Scanner(System.in);
-        var inString = scanner.nextLine();
-        var st = new StringTokenizer(inString);
-        System.out.println("Number of words: " + st.countTokens());
+        Scanner myScanner = new Scanner(System.in);
+        var inputString = myScanner.nextLine();
+        var stringTokenizer = new StringTokenizer(inputString);
+        var list = new ArrayList<Integer>();
 
-    }
-}
+        while(stringTokenizer.hasMoreTokens()) {
+            var word = stringTokenizer.nextToken();
+            list.add(word.length());
+        }
 
+        System.out.println(list);
 
-
-
-
-
-
-
-
-
-
+    }}
 
 
 
